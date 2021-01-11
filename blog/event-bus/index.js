9ea52d48304f4637f98a89187d6326ecac95de5b
Ali Ahmed <ali.ahmed@punchcard.io>
2021-01-10 20:29:37 -0700

    creating pods and services for event-bus and posts service and enable comms b/w them

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -15,17 +15,17 @@ app.post('/events', (req, res) => {
 
 	console.log('Event received: ', event);
 
-	axios.post('http://localhost:4000/events', event);
-	axios.post('http://localhost:4001/events', event);
-	axios.post('http://localhost:4002/events', event);
-	axios.post('http://localhost:4003/events', event);
+	axios.post('http://:post-clusterip-srv:4000/events', event);
+	// axios.post('http://localhost:4001/events', event);
+	// axios.post('http://localhost:4002/events', event);
+	// axios.post('http://localhost:4003/events', event);
 
 	res.send({ status: 'OK' });
 });
 
 app.get('/events', (req, res) => {
 	res.send(events);
-})
+});
 
 app.listen(4005, () => {
 	console.log('Event bus listening on 4005');
