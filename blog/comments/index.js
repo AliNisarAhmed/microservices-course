aa595e4b238f987445a1208cc004e8a3794e159f
Ali Ahmed <ali.ahmed@punchcard.io>
2021-01-14 23:25:00 -0700

    create deployment config for other services and enable communication between them, test with postman

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -26,7 +26,7 @@ app.post('/posts/:postId/comments', async (req, res) => {
 
 	console.log('Comment Created');
 
-	await axios.post('http://localhost:4005/events', {
+	await axios.post('http://event-bus-srv:4005/events', {
 		type: 'CommentCreated',
 		data: {
 			id: commentId,
@@ -50,7 +50,7 @@ app.post('/events', async (req, res) => {
 		const comment = comments.find(c => c.id === id);
 		comment.status = status;
 
-		await axios.post('http://localhost:4005/events', {
+		await axios.post('http://event-bus-srv:4005/events', {
 			type: 'CommentUpdated',
 			data
 		})
