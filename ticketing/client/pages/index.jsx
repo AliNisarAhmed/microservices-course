f5fa33b554f04e69b2bdeb337814dcfed7178706
Ali Ahmed <ali.nisar87@gmail.com>
2021-12-31 17:16:10 -0700

    client: sign in and sign up, using ssr

--- ticketing/client/pages/index.jsx
+++ ticketing/client/pages/index.jsx
@@ -1,3 +1,21 @@
-const Landing = () => <div><h1>Landing</h1></div>;
+import buildClient from '../api/build-client';
 
-export default Landing;
+const LandingPage = ({ currentUser }) => {
+	console.log(' I am in the component', currentUser);
+
+	return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
+};
+
+export async function getServerSideProps(context) {
+	console.log('getServerSideProps executed------');
+
+	const client = buildClient(context);
+
+	const {
+		data: { currentUser },
+	} = await client.get('/api/users/currentuser');
+
+	return { props: { currentUser } };
+}
+
+export default LandingPage;
