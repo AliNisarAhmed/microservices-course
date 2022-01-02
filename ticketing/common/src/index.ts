4e05ccb818c62906732f32dc4d584e4b0c4a9c11
Ali Ahmed <ali.nisar87@gmail.com>
2022-01-02 12:04:08 -0700

    Updates to common

--- ticketing/common/src/index.ts
+++ ticketing/common/src/index.ts
@@ -1,3 +1,4 @@
+// Re exports stuff from errors and middlewares
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
