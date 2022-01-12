56a670d5f37a146bf9ccb194de9fff8bc9570467
Ali Ahmed <ali.nisar87@gmail.com>
2022-01-11 22:06:22 -0700

    add event publisher to ticket service

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { natsWrapper } from './nats-wrapper';
+
 import { app } from './app';
 
 async function start() {
@@ -12,6 +14,20 @@ async function start() {
 	}
 
 	try {
+		await natsWrapper.connect('ticketing', 'sldjfnlskf', 'http://nats-srv:4222');
+
+		natsWrapper.client.on('close', () => {
+			console.log('NATS connection closed');
+			process.exit();
+		});
+
+		process.on('SIGINT', () => {
+			natsWrapper.client.close;
+		});
+		process.on('SIGNTER', () => {
+			natsWrapper.client.close;
+		});
+
 		await mongoose.connect(process.env.MONGO_URI!);
 		console.log('Tickets Service: Connected to Mongodb');
 	} catch (error) {
