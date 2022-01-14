7feda6e8023f6bc190f0a69a71d249d9af1b53a6
Ali Ahmed <ali.nisar87@gmail.com>
2022-01-13 22:23:53 -0700

    Updates to common

---
+++ ticketing/common/src/events/types/order-status.ts
@@ -0,0 +1,12 @@
+export enum OrderStaus {
+	// When the order has been created, but the ticket it is trying to order has not been reserved
+	Created = 'created',
+	// The ticket the order is trying to reserve has already been reserved, or when the user has cancelled the order
+	// or the order expiers before payment
+	// Cancelled is catchall
+	Cancelled = 'cancelled',
+	// The order has successfully reserved the ticket
+	AwaitingPayment = 'awaitingPayment',
+	// the order has reserved the ticket and the user has provided payment successfully
+	Complete = 'complete',
+}
