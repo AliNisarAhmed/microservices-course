3354ef8890d9d0db0a7bbfa994a8fe2457d1af39
Ali Ahmed <ali.nisar87@gmail.com>
2022-01-14 22:41:51 -0700

    Updates to common

--- ticketing/common/src/events/types/order-status.ts
+++ ticketing/common/src/events/types/order-status.ts
@@ -1,4 +1,4 @@
-export enum OrderStaus {
+export enum OrderStatus {
 	// When the order has been created, but the ticket it is trying to order has not been reserved
 	Created = 'created',
 	// The ticket the order is trying to reserve has already been reserved, or when the user has cancelled the order
