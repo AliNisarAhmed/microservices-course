db0201163965eec500b02d41302aa5650ce371b6
Ali Ahmed <ali.nisar87@gmail.com>
2022-01-26 22:47:00 -0700

    Expiration Service: Setup

---
+++ ticketing/expiration/src/index.ts
@@ -0,0 +1,38 @@
+import { natsWrapper } from './nats-wrapper';
+
+async function start() {
+
+	if (!process.env.NATS_URL) {
+		throw new Error('Tickets Service: NATS_URL must be defined');
+	}
+	if (!process.env.NATS_CLUSTER_ID) {
+		throw new Error('Tickets Service: NATS_CLUSTER_ID must be defined');
+	}
+	if (!process.env.NATS_CLIENT_ID) {
+		throw new Error('Tickets Service: NATS_CLIENT_ID must be defined');
+	}
+	try {
+		await natsWrapper.connect(
+			process.env.NATS_CLUSTER_ID,
+			process.env.NATS_CLIENT_ID,
+			process.env.NATS_URL
+		);
+
+		natsWrapper.client.on('close', () => {
+			console.log('NATS connection closed');
+			process.exit();
+		});
+
+		process.on('SIGINT', () => {
+			natsWrapper.client.close;
+		});
+		process.on('SIGNTER', () => {
+			natsWrapper.client.close;
+		});
+
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+start();
